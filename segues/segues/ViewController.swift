3b7ef43ab4880567fcac5d0d5226ebed7b25b602
Lili <educationapplication2024@gmail.com>
2019-07-29 12:15:51 -0400

    End of day_5

---
+++ segues/segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segues
+//
+//  Created by Lili on 7/26/19.
+//  Copyright © 2019 Lili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
